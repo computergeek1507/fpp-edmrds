3cc33d8e1dcca3676dc4861fa78e79bb4a6c585c
Poporacer <46263877+Poporacer@users.noreply.github.com>
2019-08-13 07:51:33 -0600

    Update plugin_setup.php
    fix file path

--- plugin_setup.php
+++ plugin_setup.php
@@ -8,7 +8,7 @@ if(isset($_POST['submit']))
         }
         else
         {
-    echo exec("python /home/fpp/media/plugins/edmrds/rds-song.py -c $name 2>&1 ");
+    echo exec("python /home/fpp/media/plugins/fpp-edmrds/rds-song.py -c $name 2>&1 ");
         }
 }
 ?>
